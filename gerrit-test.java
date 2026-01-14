fc77121baeaac7039ec565daaa76be22127892dc
Muzhanhong <1303469826@qq.com>
2026-01-14 15:52:49 +0800

    change gerrit-practice
    Change-Id: I01207d1ba87dc70274c392a3dbee04516501ac8b

---
+++ gerrit-test.java
@@ -0,0 +1,25 @@
+@Override
+public void setActivityController(IActivityController controller, boolean imAMonkey) {
+    mAmInternal.enforceCallingPermission(android.Manifest.permission.SET_ACTIVITY_WATCHER,
+            "setActivityController()");
+    synchronized (mGlobalLock) {
+        mController = controller;
+        mControllerIsAMonkey = imAMonkey;
+
+        if (mControllerIsAMonkey) {
+            mTrueTime = format.format(new Date());
+            Slog.d(TAG, "setActivityController imAMonkey is true, "
+                    + "calling pid = " + Binder.getCallingPid()
+                    + ", calling uid = " + Binder.getCallingUid()
+                    + ", callstack = " + Log.getStackTraceString(new Throwable()));
+        } else {
+            mFalseTime = format.format(new Date());
+            Slog.d(TAG, "setActivityController imAMonkey is false, "
+                    + "calling pid = " + Binder.getCallingPid()
+                    + ", calling uid = " + Binder.getCallingUid()
+                    + ", callstack = " + Log.getStackTraceString(new Throwable()));
+        }
+
+        Watchdog.getInstance().setActivityController(controller);
+    }
+}
