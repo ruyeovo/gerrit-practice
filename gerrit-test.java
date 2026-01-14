b6ab84af8d293d4d87c9b08deb84cc742e5dab82
Muzhanhong <1303469826@qq.com>
2026-01-14 15:16:52 +0800

    add gerrit-test
    Change-Id: I34e6374f10812226abf4c602b6597cb80fa9a069

---
+++ gerrit-test.java
@@ -0,0 +1,13 @@
+@Override
+public void setActivityController(IActivityController controller, boolean imAMonkey) {
+    mAmInternal.enforceCallingPermission(
+            android.Manifest.permission.SET_ACTIVITY_WATCHER,
+            "setActivityController()");
+
+    synchronized (mGlobalLock) {
+        mController = controller;
+        mControllerIsAMonkey = imAMonkey;
+    }
+
+    Watchdog.getInstance().setActivityController(controller);
+}
